48373e7760bcb5b547556f5d54010273c58a5ee5
neeteshgolhani <neeteshgolhani4@gmail.com>
2023-03-03 23:32:45 +0530

    Added:EmployeeWageMonthly

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,34 +1,46 @@
 package com.bridgelabz;
-import javax.management.MBeanAttributeInfo;
+
 import java.util.Random;
+
 public class EmployeeWage {
 
-        public static final int FULL_TIME = 2;
-        public static final int PART_TIME = 1;
+    public static final int FULL_TIME = 2;
+    public static final int PART_TIME = 1;
+    public static final int WORKING_DAYS = 20;
+    public static final int WAGE_PER_HR = 20;
 
-        public static void main(String[] args) {
-            System.out.println("Welcome");
-            System.out.println("Welcome to the EmployeeWageComputationalProgram");
+    public static void main(String[] args) {
+        System.out.println("Welcome to employeewage problem");
 
-            Random random = new Random();
-            int present = random.nextInt(3);
-            //Check if employee is present
-            int WagePerHr = 20;
-            int dailyWage, workingHrs;
-            switch (present){
-                case FULL_TIME :
+        Random random = new Random();
+        int present = random.nextInt(3);
+        //Check if employee is present
+        int totalWage = 0;
+        int WagePerHr = 20;
+        int dailyWage;
+
+        for (int day = 1; day <= WORKING_DAYS; day++) {
+            present = random.nextInt(3);
+            int workingHrs = 0;
+
+            switch (present) {
+                case FULL_TIME:
                     workingHrs = 8;
                     System.out.println("Employee Working Fullday");
                     break;
                 case PART_TIME:
-                    workingHrs =4;
+                    workingHrs = 4;
                     System.out.println("Employee Working Part Time");
                     break;
                 default:
                     workingHrs = 0;
                     System.out.println("Employee Absent");
             }
-            dailyWage = workingHrs * WagePerHr;
-            System.out.println("Daily wage is " + dailyWage);
+
+            dailyWage = workingHrs * WAGE_PER_HR;
+            System.out.println("Day " + day + " wage is:" + dailyWage);
+            totalWage += dailyWage;
+        }
+        System.out.println("Total wage for a month is " + totalWage);
     }
 }
