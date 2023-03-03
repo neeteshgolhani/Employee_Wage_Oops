cb31b4eb050f861691f733d2a3aeb8cca686d945
neeteshgolhani <neeteshgolhani4@gmail.com>
2023-03-03 23:11:44 +0530

    Added:EmployeeWage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -2,16 +2,18 @@ package com.bridgelabz;
 import java.util.Random;
 public class EmployeeWage {
     public static void main(String[] args) {
-        System.out.println("Welcome to the EmployeeWageComputationalProgram"
-                System.out.println("Welcome");
+        System.out.println("Welcome to the EmployeeWageComputationalProgram")
+        Random random = new Random();
+        int present = random.nextInt(2);
+        //Check if employee is present
+        int WagePerHr =20;
+        int wrkHrs = 8;
+        int dailyWage = present * WagePerHr * wrkHrs;
 
-                Random random = new Random();
-                int present = random.nextInt(2);
-                //Check if employee is present
-                if (present == 1)
-                    System.out.println("Employee is present.");
-                else
-                    System.out.println("Employee is absent.");
+        if (present == 1)
+            System.out.println("Employee is present and Daily Wage Is " + dailyWage);
+        else
+            System.out.println("Employee is absent so Daily Wage is "+ dailyWage);
             }
         }
 
