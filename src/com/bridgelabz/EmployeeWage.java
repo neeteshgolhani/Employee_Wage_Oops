948592cd59a096f0d81fcc2d3645653e5ff59bb6
neeteshgolhani <neeteshgolhani4@gmail.com>
2023-03-03 22:55:11 +0530

    Added:EmployeeAttendance

---
+++ src/com/bridgelabz/EmployeeWage.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+import java.util.Random;
+public class EmployeeWage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the EmployeeWageComputationalProgram"
+                System.out.println("Welcome");
+
+                Random random = new Random();
+                int present = random.nextInt(2);
+                //Check if employee is present
+                if (present == 1)
+                    System.out.println("Employee is present.");
+                else
+                    System.out.println("Employee is absent.");
+            }
+        }
+
