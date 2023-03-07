3f535aff20f7774b02ec6aeb7d4e0d2a5cb00c8e
neeteshgolhani <neeteshgolhani4@gmail.com>
2023-03-07 18:54:26 +0530

    Added:Employee Is Present Or Absent

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -4,8 +4,15 @@ import java.util.Random;
 
 public class EmployeeWage {
     public static void main(String[] args) {
+        Random rand = new Random();
+        int attendance = rand.nextInt(2);
+        if ( attendance == 0 ) {
+            System.out.println("Employee is present");
+        }
+        else{
+            System.out.println("Employee is absent");
+
+        }
         System.out.println("Welcome To Employee Wage Computation Problems");
-
     }
-
-    }
+}
