ce2d8f14ac8e46dffb07e4b8824c180600814456
neeteshgolhani <neeteshgolhani4@gmail.com>
2023-03-10 15:05:25 +0530

    Added:Calculate wages for a month

--- src/com/bridgelabz/Employee_Wage_computation.java
+++ src/com/bridgelabz/Employee_Wage_computation.java
@@ -3,27 +3,26 @@ import java.util.Random;
 public class Employee_Wage_computation {
     public static void main(String[] args) {
         System.out.println("welcome to employee wage computational problems" );
+        int wagePerHour = 10; // $10 per hour
+        int workingDaysPerMonth = 20; // 20 working days per month
         // create a random number generator
         Random random = new Random();
         // generate a random number between 0 and 2
         int randomNumber = random.nextInt(3);
 
-        // use switch statement to check the employee status
-        switch (randomNumber) {
-            case 0:
-                System.out.println("Employee is absent.");
-                break;
-            case 1:
-                int wage = 8 * 10; // 8 hours * $10/hour
-                System.out.println("Employee is a part-time worker. Wage: $" + wage);
-                break;
-            case 2:
-                wage = 40 * 10; // 40 hours * $10/hour
-                System.out.println("Employee is a full-time worker. Wage: $" + wage);
-                break;
-            default:
-                System.out.println("Invalid status");
-                break;
+        // check if the employee is full-time or part-time
+        if (randomNumber == 0) { // full-time employee
+            int workingHoursPerDay = 8; // 8 hours per day
+
+            int wagePerMonth = wagePerHour * workingHoursPerDay * workingDaysPerMonth;
+
+            System.out.println("Full-time employee wage per month: $" + wagePerMonth);
+        } else { // part-time employee
+            int workingHoursPerDay = 4; // 4 hours per day
+
+            int wagePerMonth = wagePerHour * workingHoursPerDay * workingDaysPerMonth;
+
+            System.out.println("Part-time employee wage per month: $" + wagePerMonth);
 
         }
     }
