6320693d51798b3e41200f2a8539068b9d0605cf
neeteshgolhani <neeteshgolhani4@gmail.com>
2023-03-10 14:53:08 +0530

    Added:Employee Wage  Solving using switch case

--- src/com/bridgelabz/Employee_Wage_computation.java
+++ src/com/bridgelabz/Employee_Wage_computation.java
@@ -5,19 +5,26 @@ public class Employee_Wage_computation {
         System.out.println("welcome to employee wage computational problems" );
         // create a random number generator
         Random random = new Random();
-
         // generate a random number between 0 and 2
         int randomNumber = random.nextInt(3);
 
-        // check if the random number is 0 (absent), 1 (part-time), or 2 (full-time)
-        if (randomNumber == 0) {
-            System.out.println("Employee is absent.");
-        } else if (randomNumber == 1) {
-            int wage = 8 * 10; // 8 hours * $10/hour
-            System.out.println("Employee is a part-time worker. Wage: $" + wage);
-        } else {
-            int wage = 40 * 10; // 40 hours * $10/hour
-            System.out.println("Employee is a full-time worker. Wage: $" + wage);
+        // use switch statement to check the employee status
+        switch (randomNumber) {
+            case 0:
+                System.out.println("Employee is absent.");
+                break;
+            case 1:
+                int wage = 8 * 10; // 8 hours * $10/hour
+                System.out.println("Employee is a part-time worker. Wage: $" + wage);
+                break;
+            case 2:
+                wage = 40 * 10; // 40 hours * $10/hour
+                System.out.println("Employee is a full-time worker. Wage: $" + wage);
+                break;
+            default:
+                System.out.println("Invalid status");
+                break;
+
         }
     }
 }
